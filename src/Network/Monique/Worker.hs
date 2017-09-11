3bc3bdf04e0e4d9f417f9f72e53dd03ed1ced333
Bogdan Neterebskii <bog2dan1@gmail.com>
2017-09-11 12:43:30 +0300

    throwWorkerError

--- src/Network/Monique/Worker.hs
+++ src/Network/Monique/Worker.hs
@@ -1,11 +1,12 @@
 module Network.Monique.Worker
-  ( UserId, TaskMessage, Version, Processing, WorkerResult (..), UType, UData, TaskResult (..)
-  , runApp, moniqueHost
+  ( UserId, TaskMessage, Version, Processing, WorkerResult (..)
+  , UType, UData, TaskResult (..)
+  , runApp, moniqueHost, throwWorkerError
   ) where
 
 import           Network.Monique.Core                  (UserId)
 import           Network.Monique.Core.Data             (TaskMessage, Version)
 import           Network.Monique.Worker.Internal.App   (moniqueHost, runApp)
 import           Network.Monique.Worker.Internal.Types (Processing, UData, TaskResult (..),
-                                                        UType,
+                                                        UType, throwWorkerError,
                                                         WorkerResult (..))
--- src/Network/Monique/Worker/Internal/Types.hs
+++ src/Network/Monique/Worker/Internal/Types.hs
@@ -1,17 +1,22 @@
 module Network.Monique.Worker.Internal.Types
   ( Processing, WorkerResult (..), UType, UData, Version, TaskResult (..)
+  , throwWorkerError
   ) where
 
-import           Control.Monad.Except       (ExceptT)
+import           Control.Monad.Except       (ExceptT, throwError)
 import           Network.Monique.Core       (UserId)
 import           Network.Monique.Core.Data  (TaskResult (..), UData, UType,
                                              Version)
-import           Network.Monique.Core.Error (MoniqueError)
+import           Network.Monique.Core.Error (MoniqueError (..))
 
 
+type WorkerName = String
 
 data WorkerResult = WorkerResult { taskResult   :: TaskResult
                                  , userdataList :: [(UType, UData)]
                                  } deriving (Show)
 
 type Processing a = UserId -> a -> ExceptT MoniqueError IO WorkerResult
+
+throwWorkerError :: WorkerName -> String -> ExceptT MoniqueError IO WorkerResult
+throwWorkerError workerName = throwError . WorkerError workerName
